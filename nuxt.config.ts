d51b5d4eaf7e5297772c394f6b3650bcbdff1495
Tougrel <lambrosagris@gmail.com>
2025-03-18 23:36:50 +0200

    style(nuxt): format nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,56 +1,56 @@
 export default defineNuxtConfig({
- modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/seo", "nuxt-delay-hydration"],
+	modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/seo", "nuxt-delay-hydration"],
 
- runtimeConfig: {
-					public: {
-									usesExternalAPI: false,
-									externalAPIAddress: process.env.EXTERNAL_API_ADDRESS,
-					},
+	runtimeConfig: {
+		public: {
+			usesExternalAPI: false,
+			externalAPIAddress: process.env.EXTERNAL_API_ADDRESS,
+		},
 	},
 
- delayHydration: {
-					mode: "mount",
-					debug: process.env.NODE_ENV === "development",
+	delayHydration: {
+		mode: "mount",
+		debug: process.env.NODE_ENV === "development",
 	},
 
- site: {
-					url: process.env.SITE_URL,
-					name: process.env.SITE_NAME,
-					description: process.env.SITE_DESCRIPTION || undefined,
-					// this is not needed if you are planning on using @nuxtjs/i18n
-					defaultLocale: process.env.SITE_LOCALE || undefined,
+	site: {
+		url: process.env.SITE_URL,
+		name: process.env.SITE_NAME,
+		description: process.env.SITE_DESCRIPTION || undefined,
+		// this is not needed if you are planning on using @nuxtjs/i18n
+		defaultLocale: process.env.SITE_LOCALE || undefined,
 	},
 
- fonts: {
-					priority: ["bunny", "google"],
-					defaults: {
-									weights: [400, 700],
-					},
+	fonts: {
+		priority: ["bunny", "google"],
+		defaults: {
+			weights: [400, 700],
+		},
 	},
 
- // this is going to make your bundle size explode. If you are on cloudflare's free tier
+	// this is going to make your bundle size explode. If you are on cloudflare's free tier
 	// then you won't be able to use it with the 1MB limit workers have
 	ogImage: {
-					enabled: false,
+		enabled: false,
 	},
 
- sitemap: {
-					enabled: false,
+	sitemap: {
+		enabled: false,
 	},
 
- robots: {
-					enabled: true,
+	robots: {
+		enabled: true,
 	},
 
- schemaOrg: {
-					enabled: false,
+	schemaOrg: {
+		enabled: false,
 	},
 
- linkChecker: {
-					enabled: true,
-					excludeLinks: [],
-					skipInspections: [],
+	linkChecker: {
+		enabled: true,
+		excludeLinks: [],
+		skipInspections: [],
 	},
 
- compatibilityDate: "2025-03-18",
+	compatibilityDate: "2025-03-18",
 });
